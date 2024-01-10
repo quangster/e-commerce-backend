2106b67aed300bbc395a959350ad3677f0e6ff18
quangster <bdq5904@gmail.com>
2024-01-10 15:13:47 +0700

    update

--- message-queue/server.js
+++ message-queue/server.js
@@ -2,8 +2,21 @@ const MessageService = require('./src/services/consumerQueue.service')
 
 const queueName = 'test-topic'
 
-MessageService.consumerQueue(queueName).then( () => {
-    console.log(`Message consumer start ${queueName}`)
+// MessageService.consumerQueue(queueName).then( () => {
+//     console.log(`Message consumer start ${queueName}`)
+// }).catch( error => {
+//     console.error(`Error consumer queue: ${error}`)
+// })
+
+MessageService.consumerToQueueNormal(queueName).then( () => {
+    console.log(`Message consumerToQueueNormal start ${queueName}`)
+}).catch( error => {
+    console.error(`Error consumer queue: ${error}`)
+})
+
+
+MessageService.consumerToQueueFailed(queueName).then( () => {
+    console.log(`Message consumerToQueueFailed start ${queueName}`)
 }).catch( error => {
     console.error(`Error consumer queue: ${error}`)
 })
--- message-queue/src/services/consumerQueue.service.js
+++ message-queue/src/services/consumerQueue.service.js
@@ -13,7 +13,66 @@ const MessageService = {
         } catch (error) {
             console.error(`Error consumer queue: ${error}`)
         }
+    },
+
+
+    // case processing
+    consumerToQueueNormal: async(queueName) => {
+        try {
+            const { channel, connection } = await connectToRabbitMQ()
+            const notiQueue = 'notificationQueueProcess' // notification queue
+
+            channel.consume(notiQueue, msg => {
+                try {
+                    const numberTest = Math.random()
+                    console.log({ numberTest })
+                    if (numberTest < 0.8) {
+                        throw new Error('error test')
+                    }
+
+                    console.log(`RECEIVED notifications successfully processed:: '${msg.content.toString()}'`)
+                    channel.ack(msg)
+
+                } catch (error) {
+                    // console.error('RECEIVED notification error::', error)
+                    channel.nack(msg, false, false)
+                }
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    },
+
+    // case failure
+    consumerToQueueFailed: async(queueName) => {
+        try {
+            const { channel, connection } = await connectToRabbitMQ()
+            
+            const notificationExchangeDLX = 'notificationExDLX'
+            const notificationRoutingKeyDLX = 'notificationRoutingKeyDLX'
+
+            const notiQueueHandler = 'notificationQueueHotFix'
+
+            await channel.assertExchange(notificationExchangeDLX, 'direct', { durable: true })
+
+            const queueResult = await channel.assertQueue(notiQueueHandler, {
+                exclusive: false
+            })
+
+            await channel.bindQueue(queueResult.queue, notificationExchangeDLX, notificationRoutingKeyDLX)
+
+            await channel.consume(queueResult.queue, msgFailed => {
+                console.log(`this notifications error, pls hot fix:: '${msgFailed.content.toString()}'`)
+            }, {
+                noAck: true
+            })
+        } catch (error) {
+            console.error(error)
+            throw error
+        }
     }
+
+
 }
 
 module.exports = MessageService
